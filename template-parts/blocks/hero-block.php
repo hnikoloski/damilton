335ff797774f35927319c79fda6553df0975caf9
hNikoloski <h.nikoloski@outlook.com>
2024-07-01 23:00:46 +0200

    feat: Add dot position fields to hero block
    The code changes include adding dot position fields to the hero block in the hero-block.php file. These fields allow for dynamically positioning dots on the hero block. This addition enhances the visual appeal and interactivity of the hero section on the website.

--- template-parts/blocks/hero-block.php
+++ template-parts/blocks/hero-block.php
@@ -22,6 +22,20 @@ $bgImgUrl = esc_url($bgImg);
 
 ?>
 
-<div <?php echo $anchor; ?> class="<?php echo esc_attr($class_name); ?> h-[81rem]" style="background-image: url('<?= $bgImgUrl; ?>'); background-size: cover; background-position: center; background-repeat: no-repeat;">
-    <h1>HERO</h1>
+<div <?php echo $anchor; ?> class="<?php echo esc_attr($class_name); ?> relative h-[81rem]" style="background-image: url('<?= $bgImgUrl; ?>'); background-size: cover; background-position: center; background-repeat: no-repeat;">
+    <?php
+    // image_cta_points repeater
+    if (have_rows('image_cta_points')) :
+        while (have_rows('image_cta_points')) : the_row();
+            $dot_position_left = get_sub_field('dot_position_left');
+            $dot_position_top = get_sub_field('dot_position_top');
+    ?>
+            <div class="kd-hero-block__dot__wrapper">
+                <div class="kd-hero-block__dot__trigger absolute w-[1.4rem] h-[1.4rem] rounded-full flex items-center justify-center border-solid border-2 border-beige z-10 cursor-pointer" style="left: <?= $dot_position_left; ?>rem; top: <?= $dot_position_top; ?>rem;">
+                    <span class="w-[.8rem] h-[.8rem] bg-beige rounded-full block"></span>
+                </div>
+            </div>
+    <?php endwhile;
+    endif;
+    ?>
 </div>
